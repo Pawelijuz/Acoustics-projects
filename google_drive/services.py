652262960cd004c95c42138c5c7dd67e3a3cae74
Pawelijuz <p.urbanowicz@poczta.fm>
2025-01-06 20:30:42 +0100

    Update services.py

--- google_drive/services.py
+++ google_drive/services.py
@@ -23,7 +23,7 @@ def authenticate():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'token.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Zapisz poświadczenia do pliku token.json
         with open('token.json', 'w') as token:
